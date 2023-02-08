e08ba2d4a162f76546d7b047a53850bbdf777f58
dan-kwon <danielkwon02@gmail.com>
2023-02-07 19:25:56 -0800

    add some WIP files

---
+++ src/movie-finder/cosine_similarity.py
@@ -0,0 +1,17 @@
+def remove_punctuation(s):
+    return s.translate(str.maketrans('','',string.punctuation))
+
+def cosine_similarity(x,y):
+    
+
+class MovieFinder():
+    """
+    epsilon: percentage of time the bandit explores rather than exploits
+    counts: number of times each arm has been pulled
+    values: average units of reward observed for each arm
+    """
+    def __init__(self):
+        pass
+    def train(self, train_data):
+        
+    def show_me_a_movie(self):
