b8bc3e51d484871e4563379a3d6335c494e6ea43
dan-kwon <danielkwon02@gmail.com>
2023-02-24 15:14:54 -0800

    adding to README

---
+++ src/Nrl/__init__.py
@@ -0,0 +1,11 @@
+from ._Nrl import imdb_Dataset,Net,train_model,test_model,predict,get_pred_df,plot_losses 
+
+__all__ = [
+    imdb_Dataset,
+    Net,
+    train_model,
+    test_model,
+    predict,
+    get_pred_df,
+    plot_losses 
+]
